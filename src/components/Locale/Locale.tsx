ae0fc137c38ddda51635835da92888e1b3d36ce0
Michał Dziadowicz <michal@dziadowicz.it>
2020-10-06 17:14:06 +0200

    remove unused translations

--- src/components/Locale/Locale.tsx
+++ src/components/Locale/Locale.tsx
@@ -1,47 +1,8 @@
 import React from "react";
 import { IntlProvider } from "react-intl";
 
-import locale_AR from "@locale/ar.json";
-import locale_AZ from "@locale/az.json";
-import locale_BG from "@locale/bg.json";
-import locale_BN from "@locale/bn.json";
-import locale_CA from "@locale/ca.json";
-import locale_CS from "@locale/cs.json";
-import locale_DA from "@locale/da.json";
-import locale_DE from "@locale/de.json";
-import locale_EL from "@locale/el.json";
-import locale_ES from "@locale/es.json";
-import locale_ES_CO from "@locale/es_CO.json";
-import locale_ET from "@locale/et.json";
-import locale_FA from "@locale/fa.json";
-import locale_FR from "@locale/fr.json";
-import locale_HI from "@locale/hi.json";
-import locale_HU from "@locale/hu.json";
-import locale_HY from "@locale/hy.json";
-import locale_ID from "@locale/id.json";
-import locale_IS from "@locale/is.json";
-import locale_IT from "@locale/it.json";
-import locale_JA from "@locale/ja.json";
-import locale_KO from "@locale/ko.json";
-import locale_MN from "@locale/mn.json";
-import locale_NB from "@locale/nb.json";
-import locale_NL from "@locale/nl.json";
 import locale_PL from "@locale/pl.json";
-import locale_PT from "@locale/pt.json";
-import locale_PT_BR from "@locale/pt_BR.json";
-import locale_RO from "@locale/ro.json";
-import locale_RU from "@locale/ru.json";
-import locale_SK from "@locale/sk.json";
-import locale_SL from "@locale/sl.json";
-import locale_SQ from "@locale/sq.json";
-import locale_SR from "@locale/sr.json";
-import locale_SV from "@locale/sv.json";
-import locale_TH from "@locale/th.json";
-import locale_TR from "@locale/tr.json";
-import locale_UK from "@locale/uk.json";
-import locale_VI from "@locale/vi.json";
-import locale_ZH_HANS from "@locale/zh-Hans.json";
-import locale_ZH_HANT from "@locale/zh-Hant.json";
+
 import useLocalStorage from "@saleor/hooks/useLocalStorage";
 
 export enum Locale {
@@ -95,49 +56,49 @@ interface StructuredMessage {
 }
 type LocaleMessages = Record<string, StructuredMessage>;
 const localeData: Record<Locale, LocaleMessages> = {
-  [Locale.AR]: locale_AR,
-  [Locale.AZ]: locale_AZ,
-  [Locale.BG]: locale_BG,
-  [Locale.BN]: locale_BN,
-  [Locale.CA]: locale_CA,
-  [Locale.CS]: locale_CS,
-  [Locale.DA]: locale_DA,
-  [Locale.DE]: locale_DE,
-  [Locale.EL]: locale_EL,
+  [Locale.AR]: undefined,
+  [Locale.AZ]: undefined,
+  [Locale.BG]: undefined,
+  [Locale.BN]: undefined,
+  [Locale.CA]: undefined,
+  [Locale.CS]: undefined,
+  [Locale.DA]: undefined,
+  [Locale.DE]: undefined,
+  [Locale.EL]: undefined,
   // Default language
   [Locale.EN]: undefined,
-  [Locale.ES]: locale_ES,
-  [Locale.ES_CO]: locale_ES_CO,
-  [Locale.ET]: locale_ET,
-  [Locale.FA]: locale_FA,
-  [Locale.FR]: locale_FR,
-  [Locale.HI]: locale_HI,
-  [Locale.HU]: locale_HU,
-  [Locale.HY]: locale_HY,
-  [Locale.ID]: locale_ID,
-  [Locale.IS]: locale_IS,
-  [Locale.IT]: locale_IT,
-  [Locale.JA]: locale_JA,
-  [Locale.KO]: locale_KO,
-  [Locale.MN]: locale_MN,
-  [Locale.NB]: locale_NB,
-  [Locale.NL]: locale_NL,
+  [Locale.ES]: undefined,
+  [Locale.ES_CO]: undefined,
+  [Locale.ET]: undefined,
+  [Locale.FA]: undefined,
+  [Locale.FR]: undefined,
+  [Locale.HI]: undefined,
+  [Locale.HU]: undefined,
+  [Locale.HY]: undefined,
+  [Locale.ID]: undefined,
+  [Locale.IS]: undefined,
+  [Locale.IT]: undefined,
+  [Locale.JA]: undefined,
+  [Locale.KO]: undefined,
+  [Locale.MN]: undefined,
+  [Locale.NB]: undefined,
+  [Locale.NL]: undefined,
   [Locale.PL]: locale_PL,
-  [Locale.PT]: locale_PT,
-  [Locale.PT_BR]: locale_PT_BR,
-  [Locale.RO]: locale_RO,
-  [Locale.RU]: locale_RU,
-  [Locale.SK]: locale_SK,
-  [Locale.SL]: locale_SL,
-  [Locale.SQ]: locale_SQ,
-  [Locale.SR]: locale_SR,
-  [Locale.SV]: locale_SV,
-  [Locale.TH]: locale_TH,
-  [Locale.TR]: locale_TR,
-  [Locale.UK]: locale_UK,
-  [Locale.VI]: locale_VI,
-  [Locale.ZH_HANS]: locale_ZH_HANS,
-  [Locale.ZH_HANT]: locale_ZH_HANT
+  [Locale.PT]: undefined,
+  [Locale.PT_BR]: undefined,
+  [Locale.RO]: undefined,
+  [Locale.RU]: undefined,
+  [Locale.SK]: undefined,
+  [Locale.SL]: undefined,
+  [Locale.SQ]: undefined,
+  [Locale.SR]: undefined,
+  [Locale.SV]: undefined,
+  [Locale.TH]: undefined,
+  [Locale.TR]: undefined,
+  [Locale.UK]: undefined,
+  [Locale.VI]: undefined,
+  [Locale.ZH_HANS]: undefined,
+  [Locale.ZH_HANT]: undefined
 };
 
 export const localeNames: Record<Locale, string> = {
