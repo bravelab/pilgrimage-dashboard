414250b90be00f7435452828684f0ad6bb09653c
Jakub Gondek <jakub.gondek@gmail.com>
2020-10-15 13:24:13 +0200

    Added volunteer permission group to /staff route

--- src/index.tsx
+++ src/index.tsx
@@ -260,7 +260,8 @@ const Routes: React.FC = () => {
                     permissions={[PermissionEnum.MANAGE_STAFF]}
                     permissionsGroups={[
                       PermissionGroupEnum.FULL_ACCESS,
-                      PermissionGroupEnum.CUSTOMER_SUPPORT
+                      PermissionGroupEnum.CUSTOMER_SUPPORT,
+                      PermissionGroupEnum.VOLUNTEER
                     ]}
                     path="/staff"
                     component={StaffSection}
